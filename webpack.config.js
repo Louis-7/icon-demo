031080a4ec1b3697810eb47c22de371e932ded0e
Louis-7 <louisgh.cn@gmail.com>
2021-08-17 13:02:21 +0800

    [feat] load sass instead of css

--- webpack.config.js
+++ webpack.config.js
@@ -12,12 +12,17 @@ module.exports = {
     }),
   ],
   module: {
-    rules: [
-      {
-        test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
-      },
-    ],
+    rules: [{
+      test: /\.s[ac]ss$/i,
+      use: [
+        // Creates `style` nodes from JS strings
+        "style-loader",
+        // Translates CSS into CommonJS
+        "css-loader",
+        // Compiles Sass to CSS
+        "sass-loader",
+      ],
+    }, ],
   },
   output: {
     filename: '[name].bundle.js',
